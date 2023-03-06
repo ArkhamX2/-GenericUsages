a38ce57cf9ad8e033f58383e357eeb800df5eafb
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2023-03-06 20:56:02 +0300

    Stack&&List

--- GenericUsages.App/GenericUsages.Library/GenericContainer/GenericList.cs
+++ GenericUsages.App/GenericUsages.Library/GenericContainer/GenericList.cs
@@ -10,6 +10,75 @@ namespace GenericUsages.Library
 {
     public class GenericList<T> : Container<T>
     {
-        public GenericList() { }
+        public GenericList()
+        {
+            _count = 0;
+            _head = null;
+            _tail = null;
+        }
+
+        /// <summary>
+        /// Конструктор по заданному списку
+        /// </summary>
+        /// <param name="items"></param>
+        public GenericList(List<T> items)
+        {
+            foreach (T item in items)
+                Add(item);
+        }
+
+        /// <summary>
+        /// Добавление элемента в конец списка
+        /// </summary>
+        /// <param name="data">добавляемый элемент</param>
+        public override void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (_head == null) _head = node;  //Если список пуст, то устанавливаем head и tail на один и тот же узел
+
+            else _tail.Next = node;  //Если список не пуст, то tail.Next хранит ссылку на новый узел
+
+            _tail = node;    //переустанавливаем ссылку tail на новый узел
+
+            _count++;
+        }
+
+        /// <summary>
+        /// Удаление объекта из списка
+        /// </summary>
+        /// <param name="data"> удаляемый объект</param>
+        /// <returns> false, если объект не найден </returns>
+        public override void Delete(T data)
+        {
+            Node<T> current = _head;
+            Node<T> previous = null;
+
+            while (current != null) //Перебираем все элементы списка
+            {
+                if (current.Data.Equals(data))  //Устанавливаем Next для предыдущего узла на след.узел по отношению к удалённому
+                {
+                    //не в начале
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next; //Переустанавливаем ссылку
+
+                        if (current.Next == null) //Если нашёлся в конце, то переустаналиваем хвост
+                            _tail = previous;
+                    }
+                    else
+                    {
+                        _head = _head.Next;   //Передвигаем голову на 1 вперёд
+                        if (_head == null) _tail = null;  //список пуст
+
+                    }
+                    _count--;
+                    return;
+                }
+                previous = current; //Двигаемся по списку на 1 вперёд
+                current = current.Next;
+            }
+            return;
+        }
     }
 }
--- GenericUsages.App/GenericUsages.Library/GenericContainer/GenericStack.cs
+++ GenericUsages.App/GenericUsages.Library/GenericContainer/GenericStack.cs
@@ -8,6 +8,63 @@ namespace GenericUsages.Library.GenericContainer
 {
     public class GenericStack<T> : Container<T>
     {
-        public GenericStack() { }
+
+        /// <summary>
+        /// Конструктор по списку элементов
+        /// </summary>
+        /// <param name="items"></param>
+        public GenericStack(List<T> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                Add(items[i]);
+            }
+        }
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public GenericStack()
+        {
+            _count = 0;
+            _head = null;
+            _tail = null;
+        }
+
+        /// <summary>
+        /// Добавить элемент в стек
+        /// </summary>
+        /// <param name="item">элемент</param>
+        public override void Add(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            node.Next = _head;
+            _head = node;
+            _count++;
+        }
+
+        /// <summary>
+        /// Удалить элемент из стека
+        /// </summary>
+        /// <returns>Удалённый элемент</returns>
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            Node<T> tmp = _head;
+            _head = _head.Next;
+            _count--;
+            return tmp.Data;
+        }
+
+        /// <summary>
+        /// Посмотреть вершину стека
+        /// </summary>
+        /// <returns> Вершину стека </returns>
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            return _head.Data;
+        }
     }
 }
